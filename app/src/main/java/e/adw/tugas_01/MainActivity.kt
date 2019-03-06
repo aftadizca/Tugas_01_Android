2dc8baac77162755683714b0cfb2f01ca0cf0efd
Afta Dizca Wahana <aftadizca@gmail.com>
2019-03-06 18:47:14 +0700

    add spinner

--- app/src/main/java/e/adw/tugas_01/MainActivity.kt
+++ app/src/main/java/e/adw/tugas_01/MainActivity.kt
@@ -1,10 +1,14 @@
 package e.adw.tugas_01
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.spinner_dropdown_item.*
 import java.io.Serializable
 
 const val inputMahasiswa = "DataMahasiswa"
@@ -14,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var submit:Button
     lateinit var nama: EditText
     lateinit var universitas:EditText
-    lateinit var jurusan:EditText
+    lateinit var jurusan:Spinner
     lateinit var kelaminGroup: RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +30,27 @@ class MainActivity : AppCompatActivity() {
         universitas = findViewById(R.id.univInput)
         jurusan = findViewById(R.id.jurusanInput)
         kelaminGroup = findViewById(R.id.kelaminGroup)
+
+        //create list in spinner
+        val adapter = ArrayAdapter.createFromResource(this,R.array.jurusan_array,R.layout.spinner_item)
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+        jurusan.adapter = adapter
+
+        jurusan?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                p1 as TextView
+                if(p2>0){
+                    p1.setTextColor(Color.BLACK)
+                }else{
+                    p1.setTextColor(ContextCompat.getColor(applicationContext,R.color.colorHint))
+                }
+
+            }
+        }
     }
 
     fun Submit(view: View){
@@ -33,17 +58,17 @@ class MainActivity : AppCompatActivity() {
         val idSelectedKelamin = kelaminGroup.checkedRadioButtonId
         val kelamin = findViewById<RadioButton>(idSelectedKelamin)
 
-        val dataMahasiswa = Mahasiswa.DataMahasiswa(nama.text.toString(), universitas.text.toString(), jurusan.text.toString(),kelamin.text.toString())
+        val dataMahasiswa = Mahasiswa.DataMahasiswa(nama.text.toString(), universitas.text.toString(), jurusan.selectedItem.toString(),kelamin.text.toString())
 
         //jika form terisi lengkap
-        if(!nama.text.toString().trim().isEmpty() && !universitas.text.toString().trim().isEmpty() && !jurusan.text.toString().trim().isEmpty() ){
+        if(!nama.text.toString().trim().isEmpty() && !universitas.text.toString().trim().isEmpty() && !jurusan.selectedItem.toString().trim().isEmpty() ){
             Toast.makeText(applicationContext, dataMahasiswa.toString() ,Toast.LENGTH_LONG).show()
             val intent = Intent(this,ShowDataMahasiswa::class.java).apply {
                 putExtra(inputMahasiswa, dataMahasiswa as Serializable)
             }
             startActivity(intent)
         }else{
-            Toast.makeText(applicationContext, "Please fill form!" ,Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext, "Please fill form!!!!" ,Toast.LENGTH_LONG).show()
         }
 
     }
